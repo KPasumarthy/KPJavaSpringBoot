da55d9f9229915dc0919de1ebb1019579303b821
Kailash Pasumarthy <KPasumarthy@gmail.com>
2024-10-11 12:48:27 -0400

    KP : Initial Commit KPJavaSpringBootApplication

---
+++ src/main/java/com/kpjavaspringboot/KPJavaSpringBoot/KPJavaSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.kpjavaspringboot.KPJavaSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KPJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KPJavaSpringBootApplication.class, args);
+		System.out.println("KP : KPJavaSpringBootApplication : main()");
+	}
+
+}
---
+++ src/test/java/com/kpjavaspringboot/KPJavaSpringBoot/KpJavaSpringBootApplicationTests.java
@@ -0,0 +1,15 @@
+package com.kpjavaspringboot.KPJavaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KPJavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("KP : KPJavaSpringBootApplicationTests : contextLoads()");
+
+	}
+
+}
