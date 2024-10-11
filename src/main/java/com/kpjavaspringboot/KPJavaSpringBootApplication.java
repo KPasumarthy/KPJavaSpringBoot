71e5c1b990bceab7a56637e4b9218162e8d9cafb
Kailash Pasumarthy <KPasumarthy@gmail.com>
2024-10-11 12:58:43 -0400

    KP : KPJavaSpringBootApplication package name set to 'com.kpjavaspringboot'

--- src/main/java/com/kpjavaspringboot/KPJavaSpringBootApplication.java
+++ src/main/java/com/kpjavaspringboot/KPJavaSpringBootApplication.java
@@ -1,4 +1,4 @@
-package com.kpjavaspringboot.KPJavaSpringBoot;
+package com.kpjavaspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/kpjavaspringboot/KPJavaSpringBootApplicationTests.java
+++ src/test/java/com/kpjavaspringboot/KPJavaSpringBootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kpjavaspringboot.KPJavaSpringBoot;
+package com.kpjavaspringboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
