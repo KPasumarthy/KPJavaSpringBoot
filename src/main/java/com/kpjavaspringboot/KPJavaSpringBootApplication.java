3067d62d764679deeb7927cd56a7c34df1b92e4f
Kailash Pasumarthy <KPasumarthy@gmail.com>
2024-10-28 16:06:57 -0400

    KP : Maven package org.testng.annotations.Test added!

--- src/main/java/com/kpjavaspringboot/KPJavaSpringBootApplication.java
+++ src/main/java/com/kpjavaspringboot/KPJavaSpringBootApplication.java
@@ -1,14 +1,40 @@
 package com.kpjavaspringboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@AutoConfiguration
 @SpringBootApplication
 public class KPJavaSpringBootApplication {
 
+	@Value("${app.title}") // Injecting the value of app.greeting from application.properties
+	private static String greeting;
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(KPJavaSpringBootApplication.class, args);
-		System.out.println("KP : KPJavaSpringBootApplication : main()");
+		System.out.println("KP : KPJavaSpringBootApplication : main() : " +  greeting);
 	}
 
 }
+
+//@SpringBootApplication
+//public class KPJavaSpringBootApplication extends SpringBootServletInitializer {
+//
+//	@Value("${app.title}") // Injecting the value of app.greeting from application.properties
+//	private static String greeting;
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(KPJavaSpringBootApplication.class);
+//	}
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(KPJavaSpringBootApplication.class, args);
+//		System.out.println("KP : KPJavaSpringBootApplication : main() : " +  greeting);
+//	}
+//}
