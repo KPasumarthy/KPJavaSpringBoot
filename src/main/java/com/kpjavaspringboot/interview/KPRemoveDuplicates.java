837154b4c2a8d7d0e27a9cff5cbe08b3fc5f7829
Kailash Pasumarthy <KPasumarthy@gmail.com>
2024-11-04 14:39:34 -0500

    KP : KPJavaSpringBootApplication Logger DateTime Now  Working!

--- src/main/java/com/kpjavaspringboot/interview/KPRemoveDuplicates.java
+++ src/main/java/com/kpjavaspringboot/interview/KPRemoveDuplicates.java
@@ -1,11 +1,21 @@
 package com.kpjavaspringboot.interview;
 
 
+import com.kpjavaspringboot.KPJavaSpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+
 public class KPRemoveDuplicates {
+
+    private static Logger logger = LoggerFactory.getLogger(KPJavaSpringBootApplication.class);
+
     public static void main(String[] args) {
 
         // KP : Debug Print
-        System.out.println("KP : KPMain - com.kpjavaspringboot : KPRemoveDuplicates : main()");
+        logger.info("KP : KPJavaSpringBootApplication : main() : " + LocalDateTime.now());
+        System.out.println("KP : KPMain - com.kpjavaspringboot : main() : " + LocalDateTime.now());
 
         //String input = new String("hi across beeeater Bookkeeper");
         //String[] input = {"hi", "across", "beeeater", "Bookkeeper"};
