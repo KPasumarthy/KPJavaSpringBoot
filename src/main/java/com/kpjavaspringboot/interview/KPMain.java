255e0adafeff1ee0c8baa1ac9d6b1ca7f32fa95d
Kailash Pasumarthy <KPasumarthy@gmail.com>
2024-11-04 14:38:13 -0500

    KP : KPJavaSpringBootApplication Logger DateTime Now  Working!

--- src/main/java/com/kpjavaspringboot/interview/KPMain.java
+++ src/main/java/com/kpjavaspringboot/interview/KPMain.java
@@ -1,12 +1,21 @@
 package com.kpjavaspringboot.interview;
 
 
+import com.kpjavaspringboot.KPJavaSpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+
 public class KPMain {
+
+    private static Logger logger = LoggerFactory.getLogger(KPJavaSpringBootApplication.class);
+
     public static void main(String[] args) {
 
         // KP : Debug Print
-        System.out.println("KP : KPMain - com.kpjavaspringboot : main()");
-
+        logger.info("KP : KPJavaSpringBootApplication : main() : " + LocalDateTime.now());
+        System.out.println("KP : KPMain - com.kpjavaspringboot : main() : " + LocalDateTime.now());
     }
 
 
