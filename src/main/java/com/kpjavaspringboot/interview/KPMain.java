9fe27a9288413b2d214b86817163928885a08390
Kailash Pasumarthy <KPasumarthy@gmail.com>
2025-02-28 10:54:19 -0500

    KP : KPJavaSpringBootApplication KPJavaInterview Working!

--- src/main/java/com/kpjavaspringboot/interview/KPMain.java
+++ src/main/java/com/kpjavaspringboot/interview/KPMain.java
@@ -18,27 +18,22 @@ public class KPMain {
         logger.info("KP : KPJavaSpringBootApplication : main() : " + LocalDateTime.now());
         System.out.println("KP : KPMain - com.kpjavaspringboot : main() : " + LocalDateTime.now());
 
-
         ///KP : Write program to get square of no -3,-2,-1,0,4,5,6 in sorted order
-        int[] Input = { -3,-2,-1,0,4,5,6 };
-        int n =  Input.length;
+        int[] Input = {-3, -2, -1, 0, 4, 5, 6};
+        int n = Input.length;
 
-        int[] Output = new int[n-1];
+        int[] Output = new int[n - 1];
 
         System.out.println("KP : KPMain - com.kpjavaspringboot : main() :  n = " + n);
 
         for (int i = 0; i < n - 1; i++) {
-               int tmp =  Input[i];
-                System.out.println("KP : KPMain - com.kpjavaspringboot : main() :  tmp  = " + tmp + " (tmp * tmp) = " + (tmp * tmp));
-                Output[i] = (tmp * tmp);
-            }
+            int tmp = Input[i];
+            System.out.println("KP : KPMain - com.kpjavaspringboot : main() :  tmp  = " + tmp + " (tmp * tmp) = " + (tmp * tmp) + " (int) (Math.pow(tmp, 2); " + (int) (Math.pow(tmp, 2)));
+            Output[i] = (tmp * tmp);
+        }
 
-        //Arrays.stream(Output).sorted();
         Arrays.sort(Output);
-        //System.out.printf("\t Sorted Array Output[] : %s \n", Arrays.toString(Output));
-
         System.out.println("KP : KPMain - com.kpjavaspringboot : main() :  Output = " + Arrays.toString(Output));
-
 
     }
 
