d908482e2f4e46aea2f06b390b2a7c29275c86b8
Kailash Pasumarthy <KPasumarthy@gmail.com>
2026-01-10 15:16:35 -0500

    KP : KPJavaSpringBootDocker - RESTContoller  @CrossOrigin(origins = "localhost:3000")  Enabled!

--- src/main/java/com/kpjavaspringboot/KPCRUDController.java
+++ src/main/java/com/kpjavaspringboot/KPCRUDController.java
@@ -7,11 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000") // Specify your frontend origin : KPReactJSWebApp hosted on NodeJS Server
 public class KPCRUDController {
 
     private static Logger logger = LoggerFactory.getLogger(KPCRUDController.class);
